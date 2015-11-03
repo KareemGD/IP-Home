8d387aecd702c27f5569653644da33bf511126c6
KareemGD <sephirothgd@gmail.com>
2015-11-03 11:04:46 +0200

    Create structure.js

---
+++ structure.js
@@ -0,0 +1,14 @@
+//ESP8266 Wifi module + Microcontroller
+function ReadGasSensor(); //on gas sensor data changed, send GET/POST data to RPi_IP/commands/gas.php?data=[PUT_THE_CHANGED_DATA_HERE]
+function ReadMotionSensor(); //same process, change the GET/POST data URL to /motion.php?data=[PUT_THE_CHANGED_DATA_HERE]
+function ReadLDR(); //..., /ldr.php?data=[PUT_THE_CHANGED_DATA_HERE]
+function IRTransmit(); //A Command that will be sent from the server to the Wifi module in this format -->WiFi_IP/IRT.php?TurnOn=1&TurnOff=0
+
+//Raspberry Pi
+UpdateHome.java //A service running on the background checking regularly on the database to execute the commands.
+
+/var/www/commads/gas.php //execute an Alarm if gas data is leaking.
+/var/www/commands/motion.php //in database 'home_db' table 'leds' put 1 or 0 to led_name='junk_room' based on motion data.
+/var/www/commands/ldr.php //in database 'home_db' table 'leds' put 1 or 0 to led_name='reception_room' based on ldr data
+/var/www/commands/IRT.php //send HTML request to the wifi module with the IRT with the command to be sent to the TV or Air Comditioner
+ 
