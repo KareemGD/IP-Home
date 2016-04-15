c96722d18d75e8d70d43b8ee51b8b09f42d4235b
KareemGD <sephirothgd@gmail.com>
2016-04-15 13:18:23 +0200

    Added files via upload

---
+++ ESP8266-Wifi-Module/Arduino/light_DONE_/light_DONE_.ino
@@ -0,0 +1,147 @@
+#include <ESP8266WiFi.h>
+#include <MQTT.h>
+#include <EEPROM.h>
+
+#define DEBUG 1
+#define EEP_ADDR 0
+#define MQTT_SERV "192.168.43.2"
+
+
+const char *ssid = "home_ap";
+const char *pass = "mkysga2015";
+
+WiFiClient net;
+MQTT myMqtt("", MQTT_SERV, 1883);
+
+bool stepOk = false;
+unsigned long lastMillis = 0;
+
+void connect(); // <- predefine connect() for setup()
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+  
+  EEPROM.begin(512);
+  int pinState = EEPROM.read(EEP_ADDR);
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, pinState);
+  
+  //client.connect();
+  myMqtt.setClientId("light");
+  myMqtt.onConnected(onConnect);
+  myMqtt.onDisconnected(onDisconnect);
+  myMqtt.onPublished(onPublish);
+  myMqtt.onData(callback);
+  
+  connect();
+  myMqtt.connect();
+  delay(500);
+  //myMqtt.subscribe("wifi/light_0");
+  waitOk();
+  //client.begin("192.168.8.100", net);
+
+  
+}
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Trying to reconnect in 5 seconds.");
+    delay(5000);
+  }
+
+  //Serial.print("\nconnecting...");
+  //while (!client.connect("arduino")) {
+   // Serial.print(".");
+  //}
+
+  //Serial.println("\nconnected!");
+
+  //client.subscribe("wifi/light_0");
+  // client.unsubscribe("/example");
+}
+
+void waitOk()
+{
+  while(!stepOk)
+    delay(100);
+ 
+  stepOk = false;
+}
+
+void onConnect() {
+#ifdef DEBUG
+  Serial.println("connected to MQTT server");
+#endif
+  myMqtt.subscribe("light_0");
+}
+
+void onDisconnect() {
+#ifdef DEBUG
+  Serial.println("disconnected. try to reconnect...");
+#endif
+  delay(500);
+  myMqtt.connect();
+}
+
+void onPublish() {
+#ifdef DEBUG  
+  Serial.println("published.");
+#endif
+}
+
+void callback(String& topic, String& data){
+#ifdef DEBUG
+  Serial.print("Data: ");
+  Serial.println(data);
+#endif
+
+  if(data == "info request") {
+    //send back the last state and
+      String state = "";
+      if (EEPROM.read(EEP_ADDR) == 1)
+        state = "true";
+      else
+        state = "false";
+        
+    myMqtt.publish(topic, "[{\"module\":\"light_0\",\"type\":\"switch\",\"last_state\":\""+state+"\"}]");  
+  } else if (data == "on") {
+    
+      digitalWrite(4, HIGH);
+      EEPROM.write(EEP_ADDR, 1);
+      EEPROM.commit();
+      
+  } else if (data == "off") {
+    
+      digitalWrite(4, LOW);
+      EEPROM.write(EEP_ADDR, 0); 
+      EEPROM.commit();
+      
+  } else {}
+}
+
+void loop() {
+  
+  //client.loop();
+  //delay(10); // <- fixes some issues with WiFi stability
+
+  //if(!client.connected()) {
+   // connect();
+  //}
+
+  // publish a message roughly every second.
+  /*if(millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("wifi/light_0", "world");
+  }*/
+}
+
+/*void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
+  Serial.print("incoming: ");
+  Serial.print(topic);
+  Serial.print(" - ");
+  Serial.print(payload);
+  Serial.println();
+}*/
---
+++ ESP8266-Wifi-Module/Arduino/motion/motion.ino
@@ -0,0 +1,167 @@
+#include <ESP8266WiFi.h>
+#include <MQTT.h>
+#include <EEPROM.h>
+
+#define DEBUG 1
+#define EEP_ADDR 0
+#define MQTT_SERV "192.168.43.2"
+#define PIR_PIN 4
+
+const char *ssid = "home_ap";
+const char *pass = "mkysga2015";
+
+WiFiClient net;
+MQTT myMqtt("", MQTT_SERV, 1883);
+
+volatile bool interrupt = false;
+bool tmrActive = false;
+unsigned long lastMsg = 0;
+unsigned long now = 0;
+int duration;
+
+void connect();
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+
+  EEPROM.begin(512);
+  duration = EEPROM.read(EEP_ADDR);
+  
+  myMqtt.setClientId("motion");
+  myMqtt.onConnected(onConnect);
+  myMqtt.onDisconnected(onDisconnect);
+  myMqtt.onPublished(onPublish);
+  myMqtt.onData(callback);
+  
+  connect();
+  myMqtt.connect();
+  delay(500);
+
+  //waitOk();
+
+  pinMode(PIR_PIN, INPUT_PULLUP);
+  attachInterrupt(
+        digitalPinToInterrupt(PIR_PIN),
+        motionDetected,
+        HIGH);
+}
+
+void loop() {
+    timer();
+
+    /*if(interrupt){
+        #ifdef DEBUG
+          Serial.println("Motion Detected: ");
+        #endif
+        if(!tmrActive){
+            String TOPIC = "motion_0";
+            String msg = "motion-detected";
+            myMqtt.publish(TOPIC, msg);
+            lastMsg = millis();
+            detachInterrupt(digitalPinToInterrupt(PIR_PIN));
+            tmrActive = true;
+        }
+        interrupt = false;
+    }*/
+}
+
+
+void connect() {
+  #ifdef DEBUG
+    Serial.print("checking wifi...");
+  #endif
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Trying to reconnect in 5 seconds.");
+    delay(5000);
+  }
+}
+
+void onConnect() {
+#ifdef DEBUG
+  Serial.println("connected to MQTT server");
+#endif
+  myMqtt.subscribe("motion_0");
+}
+
+void onDisconnect() {
+#ifdef DEBUG
+  Serial.println("disconnected. try to reconnect...");
+#endif
+  delay(500);
+  myMqtt.connect();
+}
+
+void onPublish() {
+#ifdef DEBUG  
+  Serial.println("published.");
+#endif
+}
+
+void callback(String& topic, String& data){
+  if(data == "info request") {
+      String dur = String(duration, DEC);
+      String msg = "[{\"module\":\"motion_0\",\"type\":\"sensor\",\"duration\":\""+dur+"\"}]";
+      myMqtt.publish(topic, msg);  
+      
+  } else if (data.substring(0, 8) == "duration") {
+      duration = getIntAfterSpace(data);
+      //duration = data.toInt();
+      EEPROM.write(EEP_ADDR, duration);
+      EEPROM.commit();
+      
+  #ifdef DEBUG
+    Serial.print("duration: ");
+    Serial.println(duration);
+  #endif
+
+  }
+}
+
+void motionDetected () {
+  //Serial.println("Interrupt");
+  if(!tmrActive){
+            String TOPIC = "motion_0";
+            String msg = "[{\"module\":\""+TOPIC+"\",\"type\":\"sensor\",\"status\":\"motion-detected\"}]";
+            myMqtt.publish(TOPIC, msg);
+            lastMsg = millis();
+            detachInterrupt(digitalPinToInterrupt(PIR_PIN));
+            tmrActive = true;
+        }
+  //interrupt = true;
+}
+
+
+void timer() {
+      if(tmrActive) {
+          if(duration != 0){
+            now = millis();
+
+            if(now - lastMsg == duration * 1000) {
+              #ifdef DEBUG
+                Serial.print("Now - LastMsg: ");
+                Serial.println(now - lastMsg);
+              #endif
+              delay(50);
+              tmrActive = false;
+              attachInterrupt(digitalPinToInterrupt(PIR_PIN), motionDetected, HIGH);
+            } 
+        }
+      }
+}
+
+int getIntAfterSpace(String& data) {
+  int x = data.indexOf(' ');
+  String value = "";
+  x = x - 1;
+  for(x; x < data.length(); x++) {
+      if(isDigit(data.charAt(x))) {
+          value += data.charAt(x);  
+      }
+  }
+#ifdef DEBUG
+  Serial.print("value: ");
+  Serial.println(value.toInt());
+#endif
+  return value.toInt();
+}
